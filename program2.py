5b69174b0993604e186328f31c1a4bbdbfa362e4
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-21 14:11:36 +0100

    program2

---
+++ program2.py
@@ -0,0 +1,24 @@
+'''
+
+program 2
+
+Write a program which perform add, sub, mult and div of two numbers.
+
+output:
+num1 = 10
+num2 = 2
+
+addition = 12
+subtraction = 8
+multiplication = 20
+division = 5
+
+'''
+
+number1 = int(input("Enter number1:"))
+number2 = int(input("Enter number2:"))
+
+print("Addition: ", (number1 + number2))
+print("Subtraction: ", (number1 - number2))
+print("Multiplication: ", (number1 * number2))
+print("Division: ", (number1 / number2))
