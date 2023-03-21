edb1e7496fd7f927515e21371455831bfd4edacb
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-21 14:14:58 +0100

    edit

--- program2.py
+++ program2.py
@@ -15,8 +15,8 @@ division = 5
 
 '''
 
-number1 = int(input("Enter number1:"))
-number2 = int(input("Enter number2:"))
+number1 = int(input("Enter number1: "))
+number2 = int(input("Enter number2: "))
 
 print("Addition: ", (number1 + number2))
 print("Subtraction: ", (number1 - number2))
