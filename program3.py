a1a99a76f1ba2b828242364a4d94a15602fd65e4
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-22 23:04:51 +0100

    program3

---
+++ program3.py
@@ -0,0 +1,22 @@
+'''
+
+Program3:
+
+Write a program to find squar root of number.
+
+
+
+output:
+
+number = 100
+
+squar root = 10
+
+
+'''
+
+import math
+
+number = int(input("Enter any given number: "))
+print("Square root of ", number, "is:", math.sqrt(number))
+
