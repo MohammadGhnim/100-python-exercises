4b9f8a3bdd586fa9769fc498c57123e973bd5806
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-25 17:01:06 +0100

    program5

---
+++ program5.py
@@ -0,0 +1,29 @@
+
+'''
+
+program5: write a program to print 7 times "Hello".
+
+Output:
+
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+Hello
+
+'''
+
+#method 1:
+
+print("Hello\n"*7)
+
+
+
+print('--------------------------')
+#method 2:
+
+for i in range(7):
+    print("Hello")
+
