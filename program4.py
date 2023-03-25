25e39c9c75b3ca931e75f414d410afd4d4a29795
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-25 16:41:20 +0100

    program4

---
+++ program4.py
@@ -0,0 +1,30 @@
+'''
+
+programm 4: Write a program which swap two variables.
+
+Output:
+
+
+variables:
+a = 10, b = 20
+
+variables after swapping:
+a = 20, b = 10
+
+'''
+
+
+x = 10
+y = 20
+
+print("Before Swapping...")
+print("x:", x)
+print("y:", y)
+
+x, y = y, x
+
+print("After Swapping...")
+print("x:", x)
+print("y:", y)
+
+
