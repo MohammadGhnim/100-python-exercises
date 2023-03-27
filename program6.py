72dae26f4d20925d785385ce3da33df5e79020a5
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-27 21:54:15 +0200

    program6

---
+++ program6.py
@@ -0,0 +1,22 @@
+'''
+
+program6: Write a program which generate random number.
+
+
+
+Output:
+
+random number = 4
+
+random number = 6
+
+random number = 55
+
+
+'''
+
+import random
+
+randomNumber = random.randint(1,100) #random number range
+
+print("Random Number is:", randomNumber)
