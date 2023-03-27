8c1360ec138bb38ab67a27a53687021a75f68a64
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-27 22:19:07 +0200

    program8

---
+++ program8.py
@@ -0,0 +1,17 @@
+'''
+
+program8: Write a program to find largest number from list/array.
+
+
+Output:
+
+list = [2, 3, 5, 6, 33, 5, 44, 33, 29]
+
+largest number = 4
+
+
+'''
+
+list1 = [100, 200, 33, 2, 9, 99, 999]
+
+print("Largest number is:", max(list1))
