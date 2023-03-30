0dfb1e9bff79bbafb2f3d06e7cdc35f32c24ef7b
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-30 16:19:29 +0200

    program12

---
+++ program12.py
@@ -0,0 +1,17 @@
+
+
+'''
+
+Praogram12: Write a program to print current date.
+
+Output:
+
+current date = 16/09/2019
+
+'''
+
+import datetime
+
+current_date = datetime.date.today()
+
+print("Current Date is:", current_date)
