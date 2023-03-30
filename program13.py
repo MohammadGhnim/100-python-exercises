b99484ba315f1753f460646c28064e06c9f42883
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-30 16:48:01 +0200

    program13

---
+++ program13.py
@@ -0,0 +1,24 @@
+
+
+'''
+
+
+Program13: Write a program to sort list according to aphabetic order.
+
+
+
+
+Output:
+
+list = [d, x, b, a, y, g]
+
+sortest list = [a, b, d, g, x, y]
+
+'''
+
+
+items = ['d', 'x', 'b', 'a', 'y', 'g']
+
+sorted_list = sorted(items)
+
+print("Sorted list:", sorted_list)
