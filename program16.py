99118b660a190f3daadf8b508169dbb8e0e21c37
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-03-31 23:56:25 +0200

    program16

---
+++ program16.py
@@ -0,0 +1,29 @@
+
+'''
+
+program16: Write a program to find area of triangle.
+
+
+Output:
+
+a = 5
+b = 6
+c = 7
+
+The area of triangle is 14.70
+
+
+'''
+a = 5
+b = 6
+c = 7
+
+#semi perimeter:
+
+s = (a + b + c) / 2
+
+#area:
+
+area = (s*(s-a)*(s-b)*(s-c))**0.5
+
+print("The area of the triangle is: ", area)
