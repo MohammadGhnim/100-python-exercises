34adb93e3e2c32d90379e9bc25c760d106db50c2
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-04-04 23:00:16 +0200

    program23

---
+++ program23.py
@@ -0,0 +1,15 @@
+
+
+'''
+
+program23: Write a program to sort list according to descending order.
+
+
+'''
+
+list1 = ['a', 'x', 'g', 'z', 'b']
+
+new_list = sorted(list1, reverse = True)
+
+print(list1)
+print(new_list)
