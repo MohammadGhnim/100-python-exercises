4222467efa6da5334ca223734d2db691fb31ff1b
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-04-04 22:53:18 +0200

    program22

---
+++ program22.py
@@ -0,0 +1,15 @@
+
+
+'''
+
+program22: Write a program to sort elements from list according to ascending order.
+
+
+
+'''
+
+l = ["Car", "Book", "Computer", "Bottle", "Mouse", "Laptop", "Keyboard"]
+
+new_list = sorted(l)
+
+print("sorted list:", new_list)
