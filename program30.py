f82595b86c0ad4825f958c45c6f66efef4978f7f
Mohammad Ghnim <mohammadghnim92@gmail.com>
2023-04-05 23:57:23 +0200

    program30

---
+++ program30.py
@@ -0,0 +1,11 @@
+
+
+def my_function(a, b):
+    return a + b
+
+print(my_function(2, 3, 4))
+
+
+#TypeError: my_function() takes 2 positional arguments but 3 were given
+
+#but if i remove 4, the output will be: 5
